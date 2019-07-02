9d56b55228f5ee5a6fc94115eb8089b6e6199d12
jweiming <j_weiming@hotmail.com>
2019-07-02 13:15:20 +0800

    Create polyglotAbs.java

---
+++ polyglotAbs.java
@@ -0,0 +1,39 @@
+import org.graalvm.polyglot.*;
+import org.graalvm.polyglot.proxy.*;
+
+public class PolyglotAbs {
+
+    // Embedding JavaScript
+    public static void jsAbs(int input){
+       try (Context context = Context.create("js")){
+           Value jsBindings = context.getBindings("js");
+           context.eval("js", "print('running abs in JavaScript!')");
+           System.out.println(jsBindings.getMember("Math").getMember("abs").execute(input));
+       }
+    }
+
+    // Embedding Python
+    public static void pyAbs(int input){
+       try (Context context = Context.create("python")){
+           Value pyBindings = context.getBindings("python");
+           context.eval("python", "print('running abs in Python!')");
+           System.out.println(pyBindings.getMember("abs").execute(input));
+       }
+    }
+
+    // Embedding Ruby
+    public static void rbAbs(int input){
+       try (Context context = Context.create("ruby")){
+           Value rbBindings = context.getBindings("ruby");
+           context.eval("ruby", "puts 'running abs in Ruby!'");
+           context.eval("ruby", "puts " + input + ".abs");
+       }
+    }
+
+   public static void main(String[] args) {
+        System.out.println("This polyglot example runs abs functions.");
+        jsAbs(-42);
+        pyAbs(-42);
+        rbAbs(-42);
+    }
+}
